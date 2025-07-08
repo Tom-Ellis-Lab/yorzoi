9a42524bc2a239cc4dd5a1314ea777cb61a9abe2
Timon Schneider <mail@timonschneider.de>
2025-07-08 10:07:10 +0100

    Refactor configuring model into own factory file

--- yorzoi/train.py
+++ yorzoi/train.py
@@ -25,8 +25,9 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 from yorzoi.loss import poisson_multinomial
 from yorzoi.model.baseline import DNAConvNet
-from yorzoi.config import BorzoiConfig, TrainConfig
+from yorzoi.config import TrainConfig
 from yorzoi.train_utils.data import create_datasets, create_dataloaders
+from yorzoi.train_utils.model_factory import get_model
 
 
 # Helper ---------------------------------------------------------------------
@@ -396,7 +397,7 @@ def test_model(
         f.write(json.dumps({"test_loss": test_loss}))
 
 
-def main(cfg_path: str, device: str, model_name: str, run_id: str):
+def main(cfg_path: str, device: str, run_id: str):
     import os
 
     cfg = TrainConfig.read_from_json(cfg_path)
@@ -438,43 +439,7 @@ def main(cfg_path: str, device: str, model_name: str, run_id: str):
 
     print("\tCreated dataloaders.")
 
-    # Initialize the model
-    model = None
-    if model_name == "baseline":
-        model = DNAConvNet()
-    if model_name == "clex":
-        from yorzoi.model.borzoi import Borzoi
-        from yorzoi.model.utils import TargetLengthCrop
-
-        config = BorzoiConfig(
-            dim=cfg.borzoi_cfg["dim"],
-            depth=cfg.borzoi_cfg["depth"],
-            heads=cfg.borzoi_cfg["heads"],
-            resolution=cfg.borzoi_cfg["resolution"],
-            return_center_bins_only=cfg.borzoi_cfg["return_center_bins_only"],
-            attn_dim_key=cfg.borzoi_cfg["attn_dim_key"],
-            attn_dim_value=cfg.borzoi_cfg["attn_dim_value"],
-            dropout_rate=cfg.borzoi_cfg["dropout_rate"],
-            attn_dropout=cfg.borzoi_cfg["attn_dropout"],
-            pos_dropout=cfg.borzoi_cfg["pos_dropout"],
-            enable_mouse_head=cfg.borzoi_cfg["enable_mouse_head"],
-            flashed=cfg.borzoi_cfg["flashed"],
-            separable0=cfg.borzoi_cfg["separable0"],
-            separable1=cfg.borzoi_cfg["separable1"],
-            head=cfg.borzoi_cfg["head"],
-            final_joined_convs=cfg.borzoi_cfg["final_joined_convs"],
-            upsampling_unet0=cfg.borzoi_cfg["upsampling_unet0"],
-            horizontal_conv0=cfg.borzoi_cfg["horizontal_conv0"],
-        )
-
-        model = Borzoi(config)
-
-        if cfg.checkpoint_path:
-            model.load_state_dict(torch.load(cfg.checkpoint_path, map_location="cpu"))
-
-        model.to(device)
-    else:
-        raise ValueError("Unknown model! Valid options are 'DNAConvNet' and 'clex'")
+    model = get_model(cfg)
 
     # Define loss function and optimizer
     def criterion(output, targets):
